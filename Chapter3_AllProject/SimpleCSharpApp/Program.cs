f21f8706857e1ee45bef6df42027c389f9e48f14
Shaun Mdhluli <Shaun.Mdhluli@za.ab-inbev.com>
2024-08-15 17:07:17 +0200

    Initial commit

---
+++ Chapter3_AllProject/SimpleCSharpApp/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Display a simple message to the user
+        Console.WriteLine("*********** My First C# App *********");
+        Console.WriteLine("Hello World");
+        Console.WriteLine();
+
+        //Wait for user input
+        Console.ReadLine();
+    }
+}
