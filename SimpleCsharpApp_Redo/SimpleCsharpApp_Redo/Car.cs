faede8b441761f6fa01e81788e4516b26a606888
HohenheimSA <mdhlulishaun5@gmail.com>
2025-11-09 21:50:20 +0200

    Complete exploration of Visual Studio

---
+++ SimpleCsharpApp_Redo/SimpleCsharpApp_Redo/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCsharpApp_Redo
+{
+    public class Car
+    {
+        public string PetName { get; set; }
+        public string Name { get; set; }
+
+        public int Transmission
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Make Make
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ SimpleCsharpApp_Redo/SimpleCsharpApp_Redo/Make.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCsharpApp_Redo
+{
+    public class Make
+    {
+        public string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string CountryOfOrigin
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Location
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
